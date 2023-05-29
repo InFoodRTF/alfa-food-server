aba9d04fb5310298011eb80eba3c012966476189
GamerZombar <mrzombar01@mail.ru>
2023-05-30 00:54:57 +0500

    fixes v2 :3

--- orders/views/cart.py
+++ orders/views/cart.py
@@ -111,12 +111,12 @@ class CartViewSet(ModelViewSet):
 
         menu_item = MenuItem.objects.filter(pk=menuitem_id).first()
         if menu_item is None:
-            return Response({"error": "The provided Product ID does not exist."})
+            return Response({"error": "The provided menu_item ID does not exist."})
 
         if cart.menu != menu_item.menu:
-            return Response({"error": "The provided product is not in the menu"})
+            return Response({"error": "The provided menu_item is not in the menu"})
 
-        cart_item, created = CartItem.objects.get_or_create(cart=cart, product=menu_item)
+        cart_item, created = CartItem.objects.get_or_create(cart=cart, menu_item=menu_item)
 
         if not created:
             cart_item.quantity += 1
@@ -134,7 +134,7 @@ class CartViewSet(ModelViewSet):
             return Response({"error": "Item ID not provided."}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            cart_item = CartItem.objects.get(cart=cart, product=menuitem_id)
+            cart_item = CartItem.objects.get(cart=cart, menu_item=menuitem_id)
         except CartItem.DoesNotExist:
             return Response({"error": "Item not found in cart."}, status=status.HTTP_404_NOT_FOUND)
 
